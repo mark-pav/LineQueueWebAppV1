e81a3ad8c562b65181995ee217066f1f5ed960b6
Mark Pavlyuk <mmarikk.violinist1@gmail.com>
2022-12-20 22:09:29 -0800

    connected form from /add to DB, also / displays all the entries from db

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-//jshint esversion:6
+//jshint esversion:8
+
 const express = require('express');
 var bodyParser = require('body-parser');
 let ejs = require('ejs');
@@ -8,39 +9,71 @@ const { MongoClient } = require("mongodb");
 const uri = "mongodb+srv://markuser:<password>@cluster0.igaswin.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
+const database = client.db('LineQueueBD1');
+const currentLine = database.collection('currentLine');
+
+
+
 const app = express();
 
-const line = [];
+let line = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('home');
+
+app.get('/', async function(req, res){
+  const cursor = currentLine.find();
+  line = await cursor.toArray();
+
+  res.render('home', {currentLine: line});
 });
 
 app.get('/add', function(req, res){
   res.render('add');
 });
 
+app.post('/add', function(req, res){
 
-async function run() {
-  try {
-    const database = client.db('LineQueueBD1');
-    const currentLine = database.collection('currentLine');
+  //getting values from form at /add to a local variable
+  const lineInputRequestFromUser = {
+    phoneNumber: req.body.phoneNumberInput,
+    companyName: req.body.companyNameInput,
+    description: req.body.descriptionInput,
+    equipment: req.body.equipmentInput
+  };
+  //pushing it to the local variable line array for displaying on the home page (will change to retreive from db later)
+  line.push(lineInputRequestFromUser);
 
-    // Query for a movie that has the title 'Back to the Future'
-    const doc = { phoneNumber: "253 123 1234", companyName: "best company", description: "8 drive tires" };
-    const result = await currentLine.insertOne(doc);
-    console.log(
-     `A document was inserted with the _id: ${result.insertedId}`
-  );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+  async function insertToDB(userInput) {
+
+      // const database = client.db('LineQueueBD1');
+      // const currentLine = database.collection('currentLine');
+
+      const result = await currentLine.insertOne(userInput, function(error, result){
+        if(error){
+          console.log(error);
+          alert("Looks like something went wrong while adding you to the line. Please, try again.");
+        }
+        if(result){
+          console.log(
+           `A document was inserted with the _id: ${result.insertedId}`
+          );
+            res.render("thankYou");
+        }
+      });
+
+
+      // Ensures that the client will close when you finish/error
+      //client.close();
+
   }
-}
-run().catch(console.dir);
+
+  insertToDB(lineInputRequestFromUser);
+
+});
+
+
 
 app.listen(3000, () => {
   console.log(`App listening on port 3000`);
